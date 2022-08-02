117b66ba7508ce4851fbf4d67eb32950345e2fd3
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-08-03 01:02:44 +0700

    add swiper

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ Content }) => {
   return (
     <div className="container mr-auto flex items-center justify-center">
-      <div
-        className="px-3 py-5 grid grid-cols-2 grid-rows-4 justify-center 
-        relative z-10 bg-gradient bg-gradient-to-br from-gray-300 to-transparent w-1/2 
-        bg-opacity-20 h-80 rounded-md shadow-md card"
-      ></div>
+      <div className="lg:text-2xl px-3 py-5 justify-center relative z-10 bg-gradient bg-gradient-to-br from-gray-300 to-transparent bg-opacity-20 rounded-lg shadow-2xl card border">
+        {Content}
+      </div>
     </div>
   );
 };
--- components/Hero.js
+++ components/Hero.js
@@ -3,18 +3,13 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center items-center md:space-x-12 lg:space-x-0 font-bold h-screen flex-col sm:flex-row lg:flex-col lg:space-y-4">
+    <div className="flex justify-center items-center sm:space-x-8 lg:space-x-0 font-bold h-screen flex-col sm:flex-row lg:flex-col lg:space-y-4">
       <div className="sm:w-1/3 w-2/3">
-        <img
-          src="/logo.png"
-          layout="fill"
-          alt="logo"
-          sizes="(min-width:600px) 25vw, 100vw"
-        />
+        <img src="/logo.png" alt="logo" />
       </div>
-      <div className="space-y-8 md:space-y-4">
+      <div className="space-y-4">
         <div className="flex flex-col items-center">
-          <h1 className="text-xl font-Willywonka md:text-4xl lg:text-6xl">
+          <h1 className="text-2xl font-Willywonka md:text-4xl lg:text-6xl">
             TEC{"\u00a0\u00a0"}Internship{"\u00a0\u00a0"}2022
           </h1>
           <h2 className="text-sm md:text-base lg:text-3xl font-WorkSans">
@@ -22,7 +17,7 @@ const Hero = () => {
           </h2>
         </div>
         <div className="flex justify-center">
-          <button className="bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] sm:rounded-3xl px-2 py-1 md:text-xl lg:text-3xl lg:px-4 lg:py-2 border hover:shadow-2xl">
+          <button className="bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] sm:rounded-3xl px-4 py-2 md:text-xl lg:text-3xl border hover:shadow-2xl">
             Join Us!
           </button>
         </div>
