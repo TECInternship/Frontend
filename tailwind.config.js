63252b7e2771f87f999f244991cb7f3db9e2e6ec
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-10 23:37:13 +0700

    alumni animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         Willywonka: "'Willywonka'",
         WorkSans: "'Work Sans', sans-serif",
       },
+      animation: {
+        pulse: 'pulse 0.1s cubic-bezier(0.3, 0, 1, 1) 0s',
+      },
+      keyframes: {
+        pulse: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
