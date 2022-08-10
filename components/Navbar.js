0e64e8bbbcbc87e52ad9cdd34a03e77342c7edbd
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-08-10 22:07:53 +0700

    sosmed di footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
         height={35}
         alt="logo"
       />
-      <button className="flex sticky top-0 z-50 h-[50px] flex items-center backdrop-blur-sm shadow-lg sm:rounded-3xl px-6 bg-clip-padding border border-gray-200 bg-gradient bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] bg-opacity-20">
+      <button className="flex sticky top-0 z-50 h-[50px] items-center backdrop-blur-sm shadow-lg sm:rounded-3xl px-4 py-2 md:px-6 md:py-3 text-xl bg-clip-padding border border-gray-200 bg-gradient bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] bg-opacity-20 font-bold">
         <Link href="/register">Register </Link>
         {/* bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] sm:rounded-3xl px-4 py-2 md:text-xl lg:text-3xl border hover:shadow-2xl */}
       </button>
