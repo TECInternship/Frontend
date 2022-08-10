183b833741ef8a66f17839fec04a5c0e54362f10
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-10 23:37:13 +0700

    alumni animation

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,6 @@ const Navbar = () => {
       />
       <button className="flex sticky top-0 z-50 h-[50px] items-center backdrop-blur-sm shadow-lg sm:rounded-3xl px-4 py-2 md:px-6 md:py-3 text-xl bg-clip-padding border border-gray-200 bg-gradient bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] bg-opacity-20 font-bold">
         <Link href="/register">Register </Link>
-        {/* bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] sm:rounded-3xl px-4 py-2 md:text-xl lg:text-3xl border hover:shadow-2xl */}
       </button>
     </div>
   );
