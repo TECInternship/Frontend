e75ad4b40bfef957c1d6fd2c94c8c0fdde46ecc9
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-11 11:01:58 +0700

    grammar again

--- components/About.js
+++ components/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <>
       <section className="flex flex-col items-center mb-32">
         <h1 className="text-4xl lg:text-5xl mb-5 font-Willywonka">
-          Who{"\u00a0\u00a0"}Are{"\u00a0\u00a0"}We?
+          Who{"\u00a0\u00a0"}We{"\u00a0\u00a0"}Are
         </h1>
         <p className="lg:text-2xl px-3 py-5 justify-center relative z-10 bg-gradient bg-gradient-to-br from-gray-300 to-transparent bg-opacity-20 rounded-lg shadow-2xl card border md:w-3/4 text-justify">
           <b>TEC</b>, abreviasi dari <b>Techno Entrepreneur Club</b>, merupakan <b>satu-satunya unit bisnis </b>yang menghimpun mahasiswa ITB yang memiliki minat <b>kewirausahaan</b>. TEC ITB menjadi wadah pembinaan karakter dan jiwa entrepreneurship bagi anggotanya serta memberikan pengaruh di bidang entrepreneurship untuk massa kampus dan masyarakat sekitar. Unit ini didirikan pada tanggal <b>19 Desember 2007.</b>
@@ -17,14 +17,14 @@ const About = () => {
 
       <section className="flex flex-col items-center mb-32">
         <h1 className="text-4xl lg:text-5xl mb-5 font-Willywonka">
-          What{"\u00a0\u00a0"}Do{"\u00a0\u00a0"}We{"\u00a0\u00a0"}Do?
+          What{"\u00a0\u00a0"}We{"\u00a0\u00a0"}Do
         </h1>
         <SwiperCard />
       </section>
 
       <section className="flex flex-col items-center justify-center mb-32">
         <h1 className="text-4xl lg:text-5xl mb-5 font-Willywonka">
-          What{"\u00a0\u00a0"}Will{"\u00a0\u00a0"}You{"\u00a0\u00a0"}Get?
+          What{"\u00a0\u00a0"}You{"\u00a0\u00a0"}Will{"\u00a0\u00a0"}Get
         </h1>
         <div className="flex flex-col gap-4 sm:flex-row w-2/5 md:gap-20 sm:w-2/3 lg:w-1/2 justify-items-center text-base italic text-center lg:text-xl">
           <div className="">
