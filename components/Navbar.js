6d3db56dc6b69d352cdc9d1dda392582d75d7498
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-08-11 20:37:43 +0700

    a bit navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
           alt="logo"
         />
       </div>
-      <button className="flex sticky top-0 z-50 h-[50px] items-center backdrop-blur-sm shadow-lg sm:rounded-3xl px-4 py-2 md:px-6 md:py-3 text-xl bg-clip-padding border border-gray-200 bg-gradient bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] bg-opacity-20 font-bold">
+      <button className="flex sticky top-0 z-50 h-[50px] items-center backdrop-blur-sm shadow-lg sm:rounded-3xl px-4 py-2 md:px-6 md:py-3 lg:text-xl bg-clip-padding border border-gray-200 bg-gradient bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] bg-opacity-20 font-bold">
         <Link href="/register">Register </Link>
       </button>
     </div>
