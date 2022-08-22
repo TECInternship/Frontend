270d09380667e9f406ad5674d65f4e7084522f42
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-08-22 22:15:42 +0700

    id line sama opsi

--- pages/personal-data/index.js
+++ pages/personal-data/index.js
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 const PersonalData = () => {
   const [name, setName] = useState("");
-  const [active, setActive] = useState("1");
+  const [active, setActive] = useState("2");
   const [bukti, setBukti] = useState("");
 
   return (
@@ -38,20 +38,36 @@ const PersonalData = () => {
 
           <div className="flex flex-col md:flex-row gap-4 lg:gap-10 px-6 sm:px-10 justify-center">
             <div className="bg-white/10 backdrop-blur-lg rounded-xl shadow-xl py-10 md:w-1/3 md:max-w-sm">
-              <h1 className="text-center text-2xl font-semibold">Registration</h1>
+              <h1 className="text-center text-2xl font-semibold">
+                Registration
+              </h1>
               <div className="pt-6 md:p-12 flex justify-center">
                 <div className="flex flex-col md:flex-row relative gap-4 w-full mx-2">
                   <div className="flex flex-row md:flex-col justify-center items-center pl-2 pr-4 md:px-0 gap-auto">
                     <Circle active={active} order="1" />
                     <div className="h-0.5 w-full md:w-0.5 md:h-32 bg-[#525926]"></div>
-                    <Circle active={active} order="2" className="self-center"/>
+                    <Circle active={active} order="2" className="self-center" />
                     <div className="h-0.5 w-full md:w-0.5 md:h-32 bg-[#525926]"></div>
                     <Circle active={active} order="3" />
                   </div>
                   <div className="flex flex-row md:flex-col justify-between text-xs sm:text-sm md:text-base">
-                    <div class={active === "1" && "my-1"} className="">Data diri</div>
-                    <div class={active === "2" && "my-1"} className="absolute w-full text-center md:relative md:w-auto md:text-left">Pembayaran</div>
-                    <div class={active === "3" && "my-1"} className="absolute w-full text-right md:relative md:w-auto md:text-left">Konfirmasi</div>
+                    <div className={active === "1" && "my-1"}>Data diri</div>
+                    <div
+                      className={
+                        active === "2" &&
+                        "my-1 absolute w-full text-center md:relative md:w-auto md:text-left"
+                      }
+                    >
+                      Pembayaran
+                    </div>
+                    <div
+                      className={
+                        active === "3" &&
+                        "my-1 absolute w-full text-right md:relative md:w-auto md:text-left"
+                      }
+                    >
+                      Konfirmasi
+                    </div>
                   </div>
                 </div>
               </div>
@@ -59,7 +75,11 @@ const PersonalData = () => {
 
             <div className="bg-white/10 backdrop-blur-lg rounded-xl shadow-xl  md:w-3/5 md:max-w-3xl">
               {active === "1" ? (
-                <Register1 setActive={setActive} name={name} setName={setName} />
+                <Register1
+                  setActive={setActive}
+                  name={name}
+                  setName={setName}
+                />
               ) : active === "2" ? (
                 <Register2 setActive={setActive} name={name} />
               ) : (
