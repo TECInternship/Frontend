cd2f553abd1608686a0a8b7760566bbf121ef34c
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-24 23:25:29 +0700

    nambah dikit

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -11,8 +11,9 @@ const Home = () => {
   const [activeButton, setActiveButton] = useState("login");
 
   return (
-    <>
-      <main className="h-screen w-screen bg-red bg-cover justify-center max-h-screen flex relative">
+    <div className="">
+      <main className="flex h-screen w-screen justify-center max-h-screen relative bg-[url('../public/assets/bg2.png')] bg-no-repeat bg-cover bg-left-bottom bg-fixed">
+        {/* <div className="bg-white h-36"> hahsah */}
         <div className="absolute left-2 top-2 px-8 py-10 z-10">
           <div className="flex gap-2 cursor-pointer">
             <Link href={"/"}>
@@ -34,13 +35,14 @@ const Home = () => {
               />
             </Link>
           </div>
-        </div>
+          </div>
+          {/* </div> */}
         <div className="h-screen basis-0 hidden md:block md:basis-2/5"></div>
         <div className="h-screen basis-full md:basis-3/5 flex items-center">
-          <div className="mx-auto space-y-8">
-            <Card Content={<img className="mx-auto hidden sm:flex" src={Icon.src} />} />
+          <div className="mx-auto space-y-8 sm:mt-0 ">
+            {/* <Card Content={<img className="mx-auto hidden sm:flex" src={Icon.src} />} /> */}
             <div className="bg-white/30 backdrop-blur-xl rounded-3xl w-auto max-w-full shadow-2xl">
-              <div className="text-center text-gray-100 pt-4">
+              <div className=" text-center text-gray-100 pt-4">
                 <button
                   className={`text-xl md:text-2xl font-extrabold px-6 py-2 mx-2 ${
                     activeButton === "login"
@@ -73,7 +75,7 @@ const Home = () => {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
