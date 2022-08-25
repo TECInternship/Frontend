65355b4650b32c854040c5899f9e40bd7a32c4f9
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-08-25 23:44:52 +0700

    checkbox, payment API

--- components/Auth/Register1.js
+++ components/Auth/Register1.js
@@ -2,21 +2,19 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
 
-export default function Register1({ setActive, name, setName, checked, setChecked }) {
+export default function Register1({
+  setActive,
+  name,
+  setName,
+  checked,
+  setChecked,
+}) {
   const [fakultas, setFakultas] = useState("");
   const [jurusan, setJurusan] = useState("");
   const [tahunMasuk, setTahunMasuk] = useState("");
   const [userId, setUserId] = useState("");
   const [user, setUser] = useState("");
   const [idline, setIdLine] = useState("");
-  const [Perwakilan, setPerwakilan]=useState("3");
-
-  const handleChange = () => {
-    setChecked(!checked);
-    if(!checked == true) {setPerwakilan("2")}
-    else {setPerwakilan("3")};
-  };
-
 
   useEffect(() => {
     setUserId(localStorage.getItem("user"));
@@ -37,6 +35,7 @@ export default function Register1({ setActive, name, setName, checked, setChecke
           setJurusan(res.data.jurusan);
           setTahunMasuk(res.data.tahunMasuk);
           setIdLine(res.data.idline);
+          setChecked(res.data.isPerwakilan);
         })
         .catch((err) => console.log(err));
     }
@@ -53,94 +52,107 @@ export default function Register1({ setActive, name, setName, checked, setChecke
         jurusan,
         tahunMasuk,
         idline,
+        isPerwakilan: checked,
       })
       .then((res) => {
-        setActive("2");
+        if (checked) {
+          setActive("2");
+        } else {
+          setActive("3");
+        }
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <div className="animate-fade">
-    <form className="px-12 py-10" onSubmit={handleSubmit}>
-      <h1 className="text-2xl font-semibold">Lengkapi Data Dirimu</h1>
-      <h2 className="mt-2">
-        Lengkapi data diri untuk melakukan finalisasi akunmu.
-      </h2>
+      <form className="px-12 py-10" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-semibold">Lengkapi Data Dirimu</h1>
+        <h2 className="mt-2">
+          Lengkapi data diri untuk melakukan finalisasi akunmu.
+        </h2>
 
-      {/* Nama */}
-      <h1 className="mt-10">Nama Lengkap</h1>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
-
-      {/* Fakultas */}
-      <h1 className="mt-4">Fakultas</h1>
-      <h2 className="text-xs font-extralight">Contoh: STEI, SBM</h2>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={fakultas}
-        onChange={(e) => setFakultas(e.target.value)}
-        required
-      />
-
-      {/* Program Studi */}
-      <h1 className="mt-4">Program Studi</h1>
-      <h2 className="text-xs font-extralight">Contoh: IF, TI, TPB</h2>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={jurusan}
-        onChange={(e) => setJurusan(e.target.value)}
-        required
-      />
-
-      {/* Tahun Masuk */}
-      <h1 className="mt-4">Tahun Masuk</h1>
-      <h2 className="text-xs font-extralight">Contoh: 2022</h2>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={tahunMasuk}
-        onChange={(e) => setTahunMasuk(e.target.value)}
-        required
-      />
-
-      {/* ID LINE */}
-      <h1 className="mt-4">ID LINE</h1>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={idline}
-        onChange={(e) => setIdLine(e.target.value)}
-        required
-      />
-
-      {/* Check box */}
-      <div className="flex items-center mt-8">
-        <input id="default-checkbox" type="checkbox" value={checked} onChange= {()=>handleChange()} className="w-4 h-4 bg-gray-100 rounded border-gray-300 focus:[#2F9685]"/>
-        <label for="default-checkbox" class="ml-2">Saya merupakan perwakilan pembayaran</label>
-      </div>
-
-      <div className="mt-8 flex flex-row-reverse justify-end gap-2">
+        {/* Nama */}
+        <h1 className="mt-10">Nama Lengkap</h1>
         <input
-          type={"submit"}
-          value="NEXT"
-          onClick={() => setActive(Perwakilan)}
-          className="px-8 md:px-12 py-2 rounded-xl text-md font-bold bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] cursor-pointer"
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
         />
-        <Link href="/auth">
-          <button className="px-8 md:px-12  py-2 rounded-xl text-md font-bold border">
-            BACK
-          </button>
-        </Link>
-      </div>
+
+        {/* Fakultas */}
+        <h1 className="mt-4">Fakultas</h1>
+        <h2 className="text-xs font-extralight">Contoh: STEI, SBM</h2>
+        <input
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={fakultas}
+          onChange={(e) => setFakultas(e.target.value)}
+          required
+        />
+
+        {/* Program Studi */}
+        <h1 className="mt-4">Program Studi</h1>
+        <h2 className="text-xs font-extralight">Contoh: IF, TI, TPB</h2>
+        <input
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={jurusan}
+          onChange={(e) => setJurusan(e.target.value)}
+          required
+        />
+
+        {/* Tahun Masuk */}
+        <h1 className="mt-4">Tahun Masuk</h1>
+        <h2 className="text-xs font-extralight">Contoh: 2022</h2>
+        <input
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={tahunMasuk}
+          onChange={(e) => setTahunMasuk(e.target.value)}
+          required
+        />
+
+        {/* ID LINE */}
+        <h1 className="mt-4">ID LINE</h1>
+        <input
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={idline}
+          onChange={(e) => setIdLine(e.target.value)}
+          required
+        />
+
+        {/* Check box */}
+        <div className="flex items-center mt-8">
+          <input
+            id="default-checkbox"
+            type="checkbox"
+            checked={checked}
+            onChange={() => {
+              setChecked(!checked);
+              // alert(checked);
+            }}
+            className="w-4 h-4 bg-gray-100 rounded border-gray-300 focus:[#2F9685]"
+          />
+          <label className="ml-2">Saya merupakan perwakilan pembayaran</label>
+        </div>
+
+        <div className="mt-8 flex flex-row-reverse justify-end gap-2">
+          <input
+            type={"submit"}
+            value="NEXT"
+            className="px-8 md:px-12 py-2 rounded-xl text-md font-bold bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] cursor-pointer"
+          />
+          <Link href="/auth">
+            <button className="px-8 md:px-12  py-2 rounded-xl text-md font-bold border">
+              BACK
+            </button>
+          </Link>
+        </div>
       </form>
-      </div>
+    </div>
   );
 }
--- components/Auth/Register3.js
+++ components/Auth/Register3.js
@@ -1,45 +1,46 @@
 import React, { useState, useEffect } from "react";
 
 const Register3 = (props) => {
-  const [Link, setLink]=useState("");
+  const [Link, setLink] = useState("");
   return (
     <div className="animate-fade">
-    <form className="px-12 py-10" onSubmit={() => props.setActive("3")}>
-      <div className="text-2xl font-semibold">Konfirmasi</div>
-      <div className="mt-2">
-        Sebagai bukti kelengkapan persyaratan, kamu diharuskan menyertakan bukti yang dikumpulkan dalam folder Google Drive.
-      </div>
+      <form className="px-12 py-10" onSubmit={() => props.setActive("3")}>
+        <div className="text-2xl font-semibold">Konfirmasi</div>
+        <div className="mt-2">
+          Sebagai bukti kelengkapan persyaratan, kamu diharuskan menyertakan
+          bukti yang dikumpulkan dalam folder Google Drive.
+        </div>
 
-      {/* Nama */}
-      <h1 className="mt-10">Link Google Drive</h1>
-      <input
-        type="text"
-        className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
-        value={Link}
-        onChange={(e) => setLink(e.target.value)}
-        required
-      />
-      <div className="mt-16 flex flex-row-reverse justify-end gap-2">
+        {/* Nama */}
+        <h1 className="mt-10">Link Google Drive</h1>
         <input
+          type="text"
+          className="mt-2 w-full md:w-4/5 p-3 rounded-2xl bg-white/20 backdrop-blur-none outline-none"
+          value={Link}
+          onChange={(e) => setLink(e.target.value)}
+          // required
+        />
+        <div className="mt-16 flex flex-row-reverse justify-end gap-2">
+          <input
             type={"submit"}
             value="FINISH"
             className="px-8 md:px-12 py-2 rounded-xl text-md font-bold bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] cursor-pointer"
           />
-        <button
-          className="px-8 md:px-12 py-2 rounded-xl text-md font-bold border"
-          onClick={() => {
-            if (checked) {
-            props.setActive("2")
-          } else {
-            props.setActive("1")
-          }
-          }}
-        >
-          BACK
-        </button>
-      </div>
+          <button
+            className="px-8 md:px-12 py-2 rounded-xl text-md font-bold border"
+            onClick={() => {
+              if (props.checked) {
+                props.setActive("2");
+              } else {
+                props.setActive("1");
+              }
+            }}
+          >
+            BACK
+          </button>
+        </div>
       </form>
-      </div>
+    </div>
   );
 };
 
