1c8ff2802cfe87d921cbc23d1bb769250eedd1d8
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-08-28 13:06:28 +0700

    poster

--- components/PreEvent.js
+++ components/PreEvent.js
@@ -14,7 +14,7 @@ const PreEvent = () => {
         <div className="text-center md:w-1/2 space-y-4 lg:text-2xl px-3 py-5 justify-center relative z-10 bg-opacity-20 bg-white/30 backdrop-blur-xl rounded-3xl shadow-2xl">
           <div className="flex flex-col">
             <h1 className="text-2xl md:text-2xl lg:text-3xl mb-5 font-Willywonka">
-              Live{"\u00a0\u00a0"}Interaction
+              Showcase{"\u00a0\u00a0"}Unit
             </h1>
             <h2 className="text-xl mb-5 md:text-base lg:text-xl">
               {'"'}Are You Ready To Be The Next Entrepreneur?{'"'}
@@ -22,17 +22,17 @@ const PreEvent = () => {
             <div className="flex flex-col justify-between w-full md:text-sm lg:text-base">
               <div className="flex justify-center items-center gap-2">
                 <SiGooglecalendar />
-                <p>Saturday, 27 August 2022</p>
+                <p>Sunday, 28 August 2022</p>
               </div>
               <div className="flex justify-center items-center gap-2">
                 <SiGooglemeet />
-                <p>via Zoom Meeting</p>
+                <p>via Youtube Channel</p>
               </div>
             </div>
           </div>
 
           <button className="bg-gradient-to-br from-[#9ADFD3] to-[#2F9685] sm:rounded-3xl px-4 py-2 border hover:shadow-2xl font-bold text-xl md:text-base lg:text-xl lg:px-6 lg:py-3 md:rounded-full">
-            <Link href="https://www.bit.ly/OHUTEC">
+            <Link href="https://youtu.be/Z00muhe17jM">
               <a target={"blank"}>JOIN NOW!</a>
             </Link>
           </button>
