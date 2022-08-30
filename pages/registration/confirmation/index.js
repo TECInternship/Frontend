edeb4d1508988a8665068fdf03fc4e6dcccc3776
ceavinr <16521304@std.stei.itb.ac.id>
2022-08-30 07:10:11 +0700

    alert on registration success

--- pages/registration/confirmation/index.js
+++ pages/registration/confirmation/index.js
@@ -39,6 +39,7 @@ export default function Registration({ token }) {
         })
         .then(() => {
           router.push("/");
+          alert("Registration completed!");
         })
         .catch((err) => console.log(err));
     }
