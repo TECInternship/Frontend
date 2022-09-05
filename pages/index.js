fb5a8ed5f7e410ce9b8f6add14b934893b18ce26
Alexander Jason <16521347@mahasiswa.itb.ac.id>
2022-09-06 00:48:11 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,6 @@ export default function Home({ token }) {
           <span className="motion-safe: animate-fade flex-col translate-x-10">
             <Hero token={token} />
           </span>
-          <PreEvent />
           <About />
         </div>
       </main>
