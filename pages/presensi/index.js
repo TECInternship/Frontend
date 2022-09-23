d10a8df5afed4e2badc8e44423e72aa4fbd1a6de
Alexander Jason <alexanderjason526@gmail.com>
2022-09-24 00:48:36 +0700

    absen day 2

--- pages/presensi/index.js
+++ pages/presensi/index.js
@@ -20,8 +20,8 @@ export default function Presensi({ token }) {
         <link rel="icon" href="/assets_intern/logo.png" />
       </Head>
       <main className="flex h-screen w-screen justify-center max-h-screen relative md:bg-[url('../public/assets_intern/bg-texture1.png')] bg-no-repeat bg-cover bg-center bg-fixed">
-        <div className="absolute left-2 top-2 px-16 py-10 z-10">
-          <div className="flex gap-2 cursor-pointer">
+        <div className="absolute left-2 px-8 md:px-16 md:py-10 z-10">
+          <div className="flex md:gap-2 cursor-pointer">
             <Link href={"/"}>
               <Image
                 className=""
@@ -32,37 +32,48 @@ export default function Presensi({ token }) {
               />
             </Link>
           </div>
+          <div className="invisible md:visible absolute w-[300px] h-[300px] md:w-[400px] md:h-[400px] xl:w-[550px] xl:h-[550px] 2xl:w-[700px] 2xl:h-[700px] top-0 left-1 opacity-50 flex">
+          <Link href={"/"}>
+            <Image
+            className=""
+            src="/assets_intern/spiderweb.png"
+            alt="logo1"
+            width={700}
+            height={600}
+              />
+              </Link>
+        </div>
         </div>
         {token ? (
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-x-[100px] 2xl:gap-x-[150px] gap-y-[50px] items-center mb-20 mt-20 mx-10">
-            <div>
-              <div className="w-5/6 h-5/6 xl:w-full xl:h-full flex mx-auto pb-12 relative">
-                <img src="/assets/portal-presensi-aktif.png" alt="" />
-                <div className="text-center absolute bottom-0 left-0 right-0 content-center text-3xl xl:text-4xl">
-                  <button className="py-2 px-6 bg-[#F57614] rounded-xl transition ease-in-out hover:bg-[#F78F3F]">
-                    <Link
-                      href="https://docs.google.com/forms/d/e/1FAIpQLSfRvuVC_ygNLTGpp9JK0Oj47dZzHm0XjKmlE-jLJ8WDFODYaw/viewform?usp=sf_link"
-                      target="_blank"
-                    >
-                      Day 1
-                    </Link>
-                  </button>
-                </div>
+          <div>
+            <div className="w-5/6 h-5/6 xl:w-full xl:h-full flex mx-auto pb-12 relative">
+              <img src="/assets/portal-presensi.png" alt="" />
+              <div className="text-center absolute bottom-0 left-0 right-0 content-center text-3xl xl:text-4xl">
+                <button
+                  className="py-2 px-6 bg-[#504A4A] rounded-xl hover:bg-[#383434] disabled:hover:bg-[#504A4A] cursor-not-allowed"
+                  disabled
+                >
+                  Day 1
+                </button>
               </div>
             </div>
-            <div>
-              <div className="w-5/6 h-5/6 xl:w-full xl:h-full flex mx-auto pb-12 relative">
-                <img src="/assets/portal-presensi.png" alt="" />
-                <div className="text-center absolute bottom-0 left-0 right-0 content-center text-3xl xl:text-4xl">
-                  <button
-                    className="py-2 px-6 bg-[#504A4A] rounded-xl hover:bg-[#383434] disabled:hover:bg-[#504A4A] cursor-not-allowed"
-                    disabled
+          </div>
+          <div>
+            <div className="w-5/6 h-5/6 xl:w-full xl:h-full flex mx-auto pb-12 relative">
+              <img src="/assets/portal-presensi-aktif.png" alt="" />
+              <div className="text-center absolute bottom-0 left-0 right-0 content-center text-3xl xl:text-4xl">
+                <button className="py-2 px-6 bg-[#F57614] rounded-xl transition ease-in-out hover:bg-[#F78F3F]">
+                  <Link
+                    href="https://bit.ly/PresensiAwalTECademyDay2"
+                    target="_blank"
                   >
                     Day 2
-                  </button>
-                </div>
+                  </Link>
+                </button>
               </div>
             </div>
+          </div>
             <div>
               <div className="w-5/6 h-5/6 xl:w-full xl:h-full flex mx-auto pb-12 relative mb-10 md:mb-0">
                 <img src="/assets/portal-presensi.png" alt="" />
